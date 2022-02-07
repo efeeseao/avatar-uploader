35c228702722e18a49c8993639deac0b2e211e16
Fernando dos Santos <nando4803@gmail.com>
2022-02-07 20:48:14 +0100

    feat: app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import { Avatar, Wrapper, ThemeProvider } from './components'
+import { sleep } from './utils/sleep'
+import { FileChunk } from './utils/file'
+
+import GlobalStyle from './styles/global'
+
 function App() {
+  const onUploadChunk = async (chunk: FileChunk) => {
+    await sleep(Math.random() * 2000)
+
+    return { value: chunk, error: null }
+  }
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <ThemeProvider>
+      <Wrapper bg="surface">
+        <Avatar
+          chunkSize={1024 * 20}
+          uploadChunk={onUploadChunk}
+          data-testid="avatar"
+        />
+      </Wrapper>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
